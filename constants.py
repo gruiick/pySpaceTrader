a5d9c2ff9ad062db9e655bf74f3af1c59692fb34
gruiick <gruiick@users.noreply.github.com>
2021-06-19 12:14:54 +0200

    switching to v0.3-dev

--- constants.py
+++ constants.py
@@ -10,7 +10,7 @@
     Lots from https://github.com/blind-coder/SpaceTrader/blob/master/SpaceTrader/src/main/java/de/anderdonau/spacetrader/Main.java
 """
 
-VERSION = "v0.2"
+VERSION = "v0.3"
 
 # width, x = 640, height, y = 480 (Tk)
 GRIDMIN = -100
