f483f0051f55cc1bd9e09984c06348c3d3a73449
gruiick <gruiick@users.noreply.github.com>
2024-07-31 19:11:39 +0200

    more testing stuff
        Id: tests/ship+items.py 1.develop.4 <- test_ship+items.py

--- tests/ship+items.py
+++ tests/ship+items.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 # coding: utf-8
 #
-# $Id: test_ship+items.py 1.develop.3 $
+# $Id: ship+items.py 1.develop.4 $
 # SPDX-License-Identifier: BSD-2-Clause
 
 """ experimentations around Ship() and Item() """
 
 import constants
 import core
+import random
+from dataclasses import dataclass
 
 
 class Item:
     """ items are define in constants.EQUIPEMENTS
     """
     def __init__(self, name, price):
-        self.name = name # constants.EQUIPEMENTS[name] ?
+        self.name = name
         self.price = price  # price depends on planet{PriceSlip}
 
     def __str__(self):
@@ -50,12 +52,61 @@ class Gadget(Item):
     def action(self):
         print(f"Doing stuff with {self.name}...")
 
+@dataclass
+class Crew():
+    def __init__(self, name, wage):
+        self.name = name
+        self.wage = wage
+        self.skills = {'Pilot': random.randint(0, 10),
+                       'Fighter': random.randint(0, 10),
+                       'Trader': random.randint(0, 10),
+                       'Engineer': random.randint(0, 10),
+                       }
 
-class Crew(Item):
-    def __init__(self, name, price, fonction):
-        # from/same as Captain() object?
-        super().__init__(name, price)
-        self.fonction = fonction
+    def __str__(self):
+        return f"{self.name} ({self.wage} Cr)"
+
+    def __repr__(self):
+        return f"{self.name}"
+
+    @property
+    def show_skills(self):
+        return f"{self.name}, {self.skills}"
+
+
+@dataclass
+class Captain(Crew):
+    """
+    This is captain speaking
+    """
+    def __init__(self):
+        self.name = 'Speaking'  # As in "This is Captain speaking...", got it?
+        super().__init__(self.name, None)
+
+        self.homeworld = None
+        self.location = None
+        self.destination = None
+        self.ship = None
+        self.account = None
+
+        self.status: {'Kills': 0,
+                      'Reputation': '',
+                      'Police Record': None,
+                      'Difficulty': None,
+                      }
+
+    @property
+    def balance(self):
+        # self.cash replace initial balance
+        _lst = []
+        liste_pods = [x for x in self.ship.cargo.keys() if self.ship.cargo[x]['value'] is not None]
+        for idx in liste_pods:
+            _lst.append(self.ship.cargo[idx]['value'])
+
+        cargo_value = sum(_lst)
+        balance = cargo_value + self.account.cash
+
+        return balance
 
 
 class Ship:
@@ -89,45 +140,24 @@ class Ship:
 
         # add equipment type by default key from constants (simpliest)
         for equipement_type in self.model.keys():
-            print(equipement_type)
+            # print(equipement_type)
             if equipement_type in constants.EQUIPEMENTS.keys():
                 for k, v in constants.EQUIPEMENTS[equipement_type].items():
                     if self.model[equipement_type] == v:
-                        print(f'adding: {k}, {v}')
                         if equipement_type == 'weapon':
                             if len(self.weapons) <= self.capacity_weapon:
                                 self.weapons.append(Weapon(k, v, 20))
+                                print(f'adding: {k}, {v}')
                         if equipement_type == 'shield':
                             if len(self.shields) <= self.capacity_shield:
                                 self.shields.append(Shield(k, v, 20))
+                                print(f'adding: {k}, {v}')
                         if equipement_type == 'gadget':
                             if len(self.gadgets) <= self.capacity_gadget:
                                 self.gadgets.append(Gadget(k, v, 'escapepod'))
-                        if equipement_type == 'crew':
-                            if len(self.crews) <= self.capacity_crew:
-                                self.crews.append(Crew(k, v, 'pilote'))
-
-        # for equipement_type in self.model.keys():
-        #     print(equipement_type)
-        #     if equipement_type in constants.EQUIPEMENTS.keys():
-        #         #print(f'type: {constants.EQUIPEMENTS[equipement_type]}')
-        #         print(f'capacity: {self.model[equipement_type]}')
-        #         if self.model[equipement_type] >= 0:
-        #             for k, v in constants.EQUIPEMENTS[equipement_type].items():
-        #                 if self.model[equipement_type] == v:
-        #                     print(f'adding: {k}, {v}')
-        #                     if equipement_type == 'weapon':
-        #                         self.pods[equipement_type] = Weapon(k, v, 20)
-        #                         self.capacity_weapon -= 1
-        #                     elif equipement_type == 'shield':
-        #                         self.pods[equipement_type] = Shield(k, v, 15)
-        #                         self.capacity_shield -= 1
-        #                     elif equipement_type == 'gadget':
-        #                         self.pods[equipement_type] = Item(k, v)
-        #                         self.capacity_gadget -= 1
-        #                     elif equipement_type == 'crew':
-        #                         self.pods[equipement_type] = Item(k, v)
-        #                         self.capacity_crew -= 1
+                                print(f'adding: {k}, {v}')
+                        # by default, there's never a crew in a brand new ship
+                        # you have to find and hire them
 
     def __str__(self):
         return f"{self.model[model]}: ({self.cargo}), ({self.xtrapods})"
@@ -164,6 +194,12 @@ class Ship:
         else:
             print(f"Vous ne pouvez pas utiliser un {item} de cette façon")
 
+    def remove_item(self, item):
+        pass
+
+    def drop_item(self, item):
+        pass
+
 
 if __name__ == "__main__":
     """ """
@@ -174,16 +210,24 @@ if __name__ == "__main__":
     pulse = Weapon("Pulse laser", 20, 20)
     shield = Shield("Bouclier", 15, 15)
     gadget = Gadget("autorepair", 50, 'autorepair')
+    crew = Crew('nemo', 500)
+
+    captain = Captain()
+    captain.ship = ship
 
     ship.add_item(pulse)
     ship.add_item(shield)
     ship.add_item(gadget)
+    ship.add_item(crew)
 
     print(f"Contenu du cargo du {ship.model}:")
     for item in ship.cargo:
         print(f'cargo; {item}')
     print(f'Capacity: {ship.capacity}')
     print(f'Pods: {ship.xtrapods}')
+
+    print(crew.show_skills)
+
 
     # print("\nUtilisation des objets:")
     # ship.use_item(pulse)
